8db02f149b5180c2af8e297bd26128dc4e669ed7
fendoudebb <15317832520@163.com>
2019-11-13 19:43:11 +0800

    [Hello Python]: numpy array

---
+++ python/numpy/hello_numpy_array.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+import numpy
+
+if __name__ == '__main__':
+    print(numpy.array([numpy.arange(3), numpy.arange(3), numpy.arange(3)]))
+    array = numpy.array([5, 10, 15])  # <class 'numpy.ndarray'>
+    print(array)
+    print(type(array))
+    print(array.dtype)
