ecea09763039b4d908b7fdd4eedb25204dca6c6c
fendoudebb <15317832520@163.com>
2020-04-03 10:41:55 +0800

    [hello algorithm]: 冒泡排序

---
+++ java/learn-algorithm/src/SortBubble.java
@@ -0,0 +1,88 @@
+import java.util.Arrays;
+
+/**
+ * 冒泡排序
+ */
+public class SortBubble {
+
+    public static void main(String[] args) {
+        int[] arr = {3, 2, -1, 4, -2};
+
+        sortProduce(arr);
+
+//        bubbleSort(arr);
+    }
+
+    private static void sortProduce(int[] arr) {
+        int temp;
+        //第一次排序，对比前4个即可
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        System.out.println("第1次排序：" + Arrays.toString(arr));
+
+        //第二次排序，对比前3个即可
+        for (int i = 0; i < arr.length - 1 - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        System.out.println("第2次排序：" + Arrays.toString(arr));
+
+        //第三次排序，对比前2个即可
+        for (int i = 0; i < arr.length - 1 - 2; i++) {
+            if (arr[i] > arr[i + 1]) {
+                temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        System.out.println("第3次排序：" + Arrays.toString(arr));
+
+        //第四次排序，对比前1个即可
+        for (int i = 0; i < arr.length - 1 - 3; i++) {
+            if (arr[i] > arr[i + 1]) {
+                temp = arr[i + 1];
+                arr[i + 1] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        System.out.println("第4次排序：" + Arrays.toString(arr));
+    }
+
+    private static void bubbleSort(int[] arr) {
+        int temp;
+        boolean flag = false;
+        for (int j = 0; j < arr.length; j++) {
+            // 若数组长度是5
+            // 第一次排序，只需要对前4个元素进行对比即可。经过第一次排序后，数组的最后一个元素已经是最大的了
+            // 第二次排序，只需要对前3个元素进行对比即可。以此类推，直到第四次排序结束，得到最后排完序的数组
+            // 所以
+            // 第一次排序的循环次数是`数组长度减一`次
+            // 第二次排序的循环次数是`数组长度减二`次
+            // 依次类推
+            for (int i = 0; i < arr.length - 1 - j; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    flag = true;
+                    temp = arr[i + 1];
+                    arr[i + 1] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+            System.out.println("第" + (j + 1) + "次排序：" + Arrays.toString(arr));
+            if (!flag) {
+//                break;
+            } else {
+                flag = false;
+            }
+        }
+    }
+
+
+}
