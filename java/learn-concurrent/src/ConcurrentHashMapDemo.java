878d2027b00cb3eaf856a5b127cfe51a8c8c1d2b
fendoudebb <15317832520@163.com>
2020-04-28 11:13:23 +0800

    [hello java]: ConcurrentHashMap

---
+++ java/learn-concurrent/src/ConcurrentHashMapDemo.java
@@ -0,0 +1,54 @@
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+public class ConcurrentHashMapDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
+        ConcurrentHashMap<String, String> map = new ConcurrentHashMap<>();
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+        map.put("key4", "value4");
+        map.merge("key5", "merge-value", (s, s2) -> {
+            System.out.println("merge: s, s2 = " + s + ", " + s2);
+            return s + "--------------------" + s2;
+        });
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            map.put("key1", "thread1");
+            countDownLatch.countDown();
+        }).start();
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            map.put("key1", "thread2");
+            countDownLatch.countDown();
+        }).start();
+
+        new Thread(() -> {
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("get=" + map.get("key1"));
+        }).start();
+
+        countDownLatch.await();
+        System.out.println(map);
+
+    }
+
+}
