6c11dffa98956a41a114a74030fc1fbf304daebd
fendoudebb <15317832520@163.com>
2020-04-28 11:41:06 +0800

    [hello rust]: 常量 & 变量 & mut & shadowing

--- rust/learn-rust/src/main.rs
+++ rust/learn-rust/src/main.rs
@@ -3,6 +3,39 @@ use rand::Rng;
 use std::cmp::Ordering;
 
 fn main() {
+    // guess()
+    shadowing();
+}
+
+fn shadowing() {
+    // 常量
+    const CONST_VAR: u8 = 255;
+    println!("CONST VAR={}", CONST_VAR);
+
+    // 变量
+    let x = 5;
+    println!("x={}", x);
+
+    // mut
+    let mut y = 10;
+    println!("y1={}", y);
+    y = 15;
+    println!("y2={}", y);
+
+    // Shadowing
+    let z = 20;
+    let z = z + 5;
+    let z = z + 5;
+    println!("z={}", z);
+
+    // let
+    let spaces = "           ";
+    println!("space1={}", spaces);
+    let spaces = spaces.len();
+    println!("space2={}", spaces);
+}
+
+fn guess() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
