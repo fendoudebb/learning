2989bd43d2d690fe0e430a095f482c8ca1e62daf
fendoudebb <15317832520@163.com>
2020-04-30 10:43:39 +0800

    [hello rust]: loop & while & for

--- rust/learn-rust/src/main.rs
+++ rust/learn-rust/src/main.rs
@@ -11,7 +11,88 @@ fn main() {
     // char_type();
     // tuple_type();
     // array_type();
-    scope();
+    // scope();
+    // if_else_if_expression()
+    // if_let_expression();
+    // loop_fn();
+    // while_fn()
+    for_fn()
+}
+
+fn for_fn() {
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("element={}", element)
+    }
+    // Range类型
+    // let b = 1..4;
+    for number in 1..4 {
+        println!("asc number={}", number);
+    }
+    for number in (1..4).rev() {
+        println!("desc number={}", number);
+    }
+}
+
+fn while_fn() {
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+
+        number -= 1;
+    }
+    println!("LIFTOFF!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+    while index < 5 {
+        println!("the value is: {}", a[index]);
+
+        index += 1;
+    }
+}
+
+fn loop_fn() {
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {}", result);
+}
+
+fn if_let_expression() {
+    let condition = false;
+    let number = if condition {
+        5
+    } else {
+        6
+    };
+    println!("The value of number is: {}", number);
+}
+
+fn if_else_if_expression() {
+    let number = 3;
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+    if number != 0 {
+        println!("number was something other than zero");
+    }
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
 }
 
 fn scope() {
