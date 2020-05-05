f69cbf907545f107256c2cef18abd77056b79c9c
fendoudebb <15317832520@163.com>
2020-05-05 09:39:30 +0800

    [learn Java]: 注释是否会被执行

---
+++ java/learn-comment/src/Comment.java
@@ -0,0 +1,18 @@
+/**
+ * zbj: created on 2020/5/5 8:17.
+ */
+public class Comment {
+
+    public static void main(String[] args) {
+        String str = "你好世界";
+        // \u000dstr="Hello World";
+        System.out.println(str);
+
+        System.out.println(\u000d);
+        System.out.println('\u03C0');
+
+        double π = Math.PI;
+        System.out.println(\u03C0);
+    }
+
+}
