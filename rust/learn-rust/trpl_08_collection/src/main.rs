205d01ee723eb0bdda1aef788bb821ea36a54d4e
fendoudebb <15317832520@163.com>
2020-05-16 09:43:59 +0800

    [hello rust]: String

--- rust/learn-rust/trpl_08_collection/src/main.rs
+++ rust/learn-rust/trpl_08_collection/src/main.rs
@@ -1,5 +1,46 @@
 #![allow(unused_variables)]
+
 fn main() {
+    // vector();
+    let mut s = String::new();
+    let data = "initial contents";
+
+    let s = data.to_string();
+
+// the method also works on a literal directly:
+    let s = "initial contents".to_string();
+
+    let mut s = String::from("foo");
+    s.push_str("bar");
+
+    let mut s = String::from("lo");
+    s.push('l');
+
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s = format!("{}-{}-{}", s1, s2, s3);
+
+    println!("s={}, s1={}, s2={}, s3={}", s, s1, s2, s3);
+
+    let hello = "Здравствуйте";
+    let s = &hello[0..4];
+
+    println!("s={}", s);
+
+    for c in "नमस्ते".chars() {
+        println!("{}", c);
+    }
+
+    for b in "नमस्ते".bytes() {
+        println!("{}", b);
+    }
+
+}
+
+fn vector() {
     let vec: Vec<i32> = Vec::new();
     println!("{:?}", vec);
 
@@ -46,5 +87,4 @@ fn main() {
     for i in &v {
         println!("{}", i);
     }
-
 }
