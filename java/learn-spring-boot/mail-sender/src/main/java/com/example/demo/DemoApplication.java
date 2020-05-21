f5f6713a6780a81ae06acb0e14a2d9585220e4ff
fendoudebb <15317832520@163.com>
2020-05-21 09:11:35 +0800

    [hello java]: JavaMailSender

--- java/learn-spring-boot/mail-sender/src/main/java/com/example/demo/DemoApplication.java
+++ java/learn-spring-boot/mail-sender/src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,20 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 
 import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeUtility;
+import javax.mail.util.ByteArrayDataSource;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class DemoApplication implements ApplicationRunner {
@@ -21,6 +31,38 @@ public class DemoApplication implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+//        sendSimpleMail();
+        sendMimeMessage();
+    }
+
+    private void sendMimeMessage() throws MessagingException, IOException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
+        mimeMessageHelper.setFrom("123@test.com");
+        mimeMessageHelper.setTo("123@test.com");//此处为调试，发给了自己
+        mimeMessageHelper.setSubject("主题3333");
+
+        ClassPathResource classPathResource = new ClassPathResource("application.yml");
+
+        File attachmentFile = classPathResource.getFile();
+        mimeMessageHelper.addAttachment(MimeUtility.encodeWord("附件1:application.yml"), attachmentFile);
+
+        ByteArrayDataSource attachmentDataSource = new ByteArrayDataSource(classPathResource.getInputStream(), "application/octet-stream");
+        mimeMessageHelper.addAttachment(MimeUtility.encodeText("附件2:application.yml"), attachmentDataSource);
+        //富文本
+        mimeMessageHelper.setText("<html><body><p>你好:</p><div><img src='cid:contentId1'></div><a href='https://www.baidu.com'>https://www.baidu.com</a></body></html>",true);
+
+        ClassPathResource classPathResource2 = new ClassPathResource("pic/test.png");
+
+        String type = Files.probeContentType(Paths.get(classPathResource2.getURI()));
+
+        ByteArrayDataSource inlineDataSource = new ByteArrayDataSource(classPathResource2.getInputStream(), type);
+
+        mimeMessageHelper.addInline("contentId1", inlineDataSource);
+        javaMailSender.send(mimeMessage);
+    }
+
+    private void sendSimpleMail() {
         //建立邮件消息
         SimpleMailMessage mainMessage = new SimpleMailMessage();
         //发送方
