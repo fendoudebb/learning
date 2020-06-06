d9fc7b445732397add915dcdd4bc5c8e68ea54b5
fendoudebb <15317832520@163.com>
2020-06-06 19:47:59 +0800

    [learn Rust]: trait

---
+++ rust/learn-rust/trpl_10_trait/src/main.rs
@@ -0,0 +1,60 @@
+use trpl_10_trait::{Summary, Tweet};
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+    notify(tweet);
+
+
+
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+}
+
+pub fn notify(item: impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// pub fn notify2(item: impl Summary + Display) {
+//     println!("Breaking news! {}", item);
+// }
+
+// fn some_function<T, U>(t: T, u: U) -> i32
+//     where T: Display + Clone,
+//           U: Clone + Debug
+// {}
+
+fn returns_summarizable() -> impl Summary {
+    Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    }
+}
+
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
