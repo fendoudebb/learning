de501de76eeba22f9b42e775b76ea6e09665e291
fendoudebb <15317832520@163.com>
2020-08-20 09:00:51 +0800

    [hello Rust]: 生命周期

--- rust/learn-rust/trpl_10_lifetime/src/main.rs
+++ rust/learn-rust/trpl_10_lifetime/src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     let string1 = String::from("abcd");
     let string2 = "xyz";
 
-    let result = longest(string1.as_str(), string2);
+    let result = long2(string1.as_str(), string2);
     println!("The longest string is {}", result);
 
     let novel = String::from("Call me Ishmael. Some years ago...");
@@ -17,6 +17,22 @@ fn main() {
         .expect("Could not find a '.'");
     let i = ImportantExcerpt { part: first_sentence };
 
+    let string1 = String::from("long string is long");
+    let result;
+    {
+        let string2 = String::from("xyz");
+        result = long2(string1.as_str(), string2.as_str());
+    }
+    println!("The longest string is {}", result);
+
+}
+
+fn long2(x: & str, y: & str) -> String {
+    if x.len() > y.len() {
+        x.to_string()
+    } else {
+        y.to_string()
+    }
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
