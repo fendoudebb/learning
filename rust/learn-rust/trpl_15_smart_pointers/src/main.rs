cc6d6f9689fc3bc673ba39222f4be7c32cf247be
fendoudebb <15317832520@163.com>
2020-08-21 09:19:31 +0800

    [hello Rust]: Deref

--- rust/learn-rust/trpl_15_smart_pointers/src/main.rs
+++ rust/learn-rust/trpl_15_smart_pointers/src/main.rs
@@ -1,3 +1,26 @@
+use std::ops::Deref;
+
 fn main() {
-    println!("Hello, world!");
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+}
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        &self.0
+    }
 }
