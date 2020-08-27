69825eadd9390a8d84fa11ac5f82f4b773c05ad7
fendoudebb <15317832520@163.com>
2020-08-27 16:46:19 +0800

    [hello Java]: Drop

--- rust/learn-rust/trpl_15_smart_pointers/src/main.rs
+++ rust/learn-rust/trpl_15_smart_pointers/src/main.rs
@@ -3,6 +3,15 @@ use std::ops::Deref;
 fn main() {
     let m = MyBox::new(String::from("Rust"));
     hello(&m);
+
+    let c = CustomSmartPointer { data: String::from("my stuff") };
+    let d = CustomSmartPointer { data: String::from("other stuff") };
+    println!("CustomSmartPointers created.");
+
+    let c = CustomSmartPointer { data: String::from("some data") };
+    println!("CustomSmartPointer created.");
+    drop(c);
+    println!("CustomSmartPointer dropped before the end of main.");
 }
 
 fn hello(name: &str) {
@@ -22,5 +31,15 @@ impl<T> Deref for MyBox<T> {
 
     fn deref(&self) -> &T {
         &self.0
+    }
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
     }
 }
