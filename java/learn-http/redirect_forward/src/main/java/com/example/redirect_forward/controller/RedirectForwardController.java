5cdbf7a54a6a3a4bd9521449073b458f6856b445
fendoudebb <15317832520@163.com>
2021-04-08 09:13:40 +0800

    [hello Spring]: redirect & forward

--- java/learn-http/redirect_forward/src/main/java/com/example/redirect_forward/controller/RedirectForwardController.java
+++ java/learn-http/redirect_forward/src/main/java/com/example/redirect_forward/controller/RedirectForwardController.java
@@ -2,7 +2,14 @@ package com.example.redirect_forward.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Controller
 public class RedirectForwardController {
@@ -26,6 +33,17 @@ public class RedirectForwardController {
     @GetMapping("/test4")
     public String test4() {
         return "forward:/test3";
+    }
+
+    @GetMapping("/test-forward")
+    public void testForward(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // request.getRequestDispatcher("https://www.baidu.com").forward(request, response); // 无法跨域 forward
+        request.getRequestDispatcher("https://www.baidu.com").forward(request, response);
+    }
+
+    @GetMapping("/test-redirect")
+    public void testRedirect(HttpServletResponse response) throws IOException {
+        response.sendRedirect("https://www.baidu.com");
     }
 
 }
