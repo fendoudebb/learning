70bcde2f3795d89b7c803d4014246f752ded7a4a
fendoudebb <15317832520@163.com>
2021-05-07 07:13:39 +0800

    [Hello Java]: Queue

---
+++ java/learn-juc/src/queue/LinkedBlockingDequeDemo.java
@@ -0,0 +1,14 @@
+package queue;
+
+import java.util.concurrent.LinkedBlockingDeque;
+
+/**
+ * zbj: created on 2021/4/12 21:11.
+ */
+public class LinkedBlockingDequeDemo {
+
+    public static void main(String[] args) {
+        LinkedBlockingDeque<String> deque = new LinkedBlockingDeque<>();
+    }
+
+}
