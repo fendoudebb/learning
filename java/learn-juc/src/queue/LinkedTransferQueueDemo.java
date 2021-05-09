fca3ddda19638f8da2fa26ed1635d9a002e225aa
fendoudebb <15317832520@163.com>
2021-05-09 08:08:08 +0800

    [Hello Java]: Queue

---
+++ java/learn-juc/src/queue/LinkedTransferQueueDemo.java
@@ -0,0 +1,14 @@
+package queue;
+
+import java.util.concurrent.LinkedTransferQueue;
+
+/**
+ * zbj: created on 2021/4/12 21:12.
+ */
+public class LinkedTransferQueueDemo {
+
+    public static void main(String[] args) {
+        LinkedTransferQueue<String> queue = new LinkedTransferQueue<>();
+    }
+
+}
