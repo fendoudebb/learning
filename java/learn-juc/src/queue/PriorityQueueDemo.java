62445ab1f42bb48ee04886571862518b5a24ecd3
fendoudebb <15317832520@163.com>
2021-05-11 07:40:20 +0800

    [Hello Java]: Queue

---
+++ java/learn-juc/src/queue/PriorityQueueDemo.java
@@ -0,0 +1,17 @@
+package queue;
+
+import java.util.PriorityQueue;
+
+/**
+ * zbj: created on 2021/4/12 21:01.
+ */
+public class PriorityQueueDemo {
+
+    public static void main(String[] args) {
+        PriorityQueue<String> queue = new PriorityQueue<>();
+        queue.add("a");
+        queue.add("b");
+        queue.add("c");
+    }
+
+}
