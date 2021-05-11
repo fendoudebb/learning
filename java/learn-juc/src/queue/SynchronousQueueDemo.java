34b1806835c317888b6bb766b8459eb884fde4cf
fendoudebb <15317832520@163.com>
2021-05-12 07:28:20 +0800

    [Hello Java]: Queue

---
+++ java/learn-juc/src/queue/SynchronousQueueDemo.java
@@ -0,0 +1,16 @@
+package queue;
+
+import java.util.concurrent.SynchronousQueue;
+
+/**
+ * zbj: created on 2021/3/28 22:03.
+ */
+public class SynchronousQueueDemo {
+
+    public static void main(String[] args) {
+        SynchronousQueue<String> queue = new SynchronousQueue<>();
+
+//        queue.
+    }
+
+}
