a6674de0570c095dd0a169389167bdb0a34dce8f
fendoudebb <15317832520@163.com>
2021-07-12 07:21:15 +0800

    [Hello JVM]: Constan Fold

---
+++ java/learn-jvm/src/ConstantFoldDemo.java
@@ -0,0 +1,14 @@
+/**
+ * zbj: created on 2021/4/7 20:51.
+ */
+public class ConstantFoldDemo {
+
+    public static void main(String[] args) {
+        int i = 2 + 3;  // iconst_5
+        int j = 3 + 3; // bipush 6
+        int k = 3 - 4; // iconst_m1
+        int l = 3 - 5; // bipush -2
+//        String j = "hello" + "world";
+    }
+
+}
