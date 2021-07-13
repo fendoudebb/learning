0cc41bc3e4090c617fc4c083a971ae3cefc37e6e
fendoudebb <15317832520@163.com>
2021-07-14 07:48:09 +0800

    [Hello JVM]: PC Register

---
+++ java/learn-jvm/src/PCRegisterDemo.java
@@ -0,0 +1,18 @@
+/**
+ * zbj: created on 2021/6/27 21:09.
+ *
+ * javap -v PCRegisterDemo.class
+ */
+public class PCRegisterDemo {
+
+    public static void main(String[] args) {
+        int i = 10;
+        int j = 20;
+        int k = i + j;
+
+        String a = "abc";
+        System.out.println(i);
+        System.out.println(k);
+    }
+
+}
